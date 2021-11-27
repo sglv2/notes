62954ab2b19dec3da206c090cb6d77900632a2a0
S G <serbangilvitu@users.noreply.github.com>
2021-11-27 14:44:58 +0400

    Updated kubeadm notes

--- k8s/kubeadm/kubeadm-download.sh
+++ k8s/kubeadm/kubeadm-download.sh
@@ -6,7 +6,10 @@ TOOLS_DOWNLOAD_DIR=/opt/k8s-tools
 DEP_DOWNLOAD_DIR=/opt/k8s-dependencies
 IMG_DOWNLOAD_DIR=/opt/k8s-images
 export PATH=${TOOLS_DOWNLOAD_DIR}:${PATH}
-# Make sure that the br_netfilter module is loaded
+
+CONTAINERD_VERSION='1.5.8'
+TEMPLATES_VERSION="v0.4.0"
+
 lsmod | grep br_netfilter || bash -c "echo br_netfilter | tee /etc/modules-load.d/k8s.conf"
 lsmod | grep overlay || bash -c "echo overlay | tee /etc/modules-load.d/containerd.conf"
 
@@ -17,8 +20,10 @@ modprobe br_netfilter
 # Download dependencies
 mkdir -p ${DEP_DOWNLOAD_DIR}
 pushd ${DEP_DOWNLOAD_DIR}
-curl -LO http://mirrors.kernel.org/ubuntu/pool/main/libs/libseccomp/libseccomp2_2.5.1-1ubuntu1~20.04.1_amd64.deb
-# dpkg -i libseccomp2*
+
+curl -sLO http://mirrors.kernel.org/ubuntu/pool/main/s/socat/socat_1.7.3.3-2_amd64.deb
+curl -sLO http://mirrors.kernel.org/ubuntu/pool/main/c/conntrack-tools/conntrack_1.4.5-2_amd64.deb
+
 popd
 
 # Download tools
@@ -29,29 +34,19 @@ ARCH="amd64"
 curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet,kubectl}
 chmod +x {kubeadm,kubelet,kubectl}
 
-RELEASE_VERSION="v0.4.0"
-curl -sSLO "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" 
-#| sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service
-#mkdir -p /etc/systemd/system/kubelet.service.d
-curl -sSLO "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" 
-#| sed "s:/usr/bin:${DOWNLOAD_DIR}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+curl -sSLO "https://raw.githubusercontent.com/kubernetes/release/${TEMPLATES_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service" 
+
+curl -sSLO "https://raw.githubusercontent.com/kubernetes/release/${TEMPLATES_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" 
 
 # Download containerd
 # https://github.com/containerd/containerd/blob/main/docs/cri/installation.md
 # https://github.com/containerd/containerd/releases
-CONTAINERD_VERSION='1.5.8'
 wget https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz
 wget https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz.sha256sum
 sha256sum --check cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz.sha256sum
 tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz
 systemctl daemon-reload
 systemctl start containerd
-
-cat << EOF > 0-containerd.conf
-# /etc/systemd/system/kubelet.service.d/0-containerd.conf
-[Service]
-Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
-EOF
 
 popd
 
@@ -61,7 +56,7 @@ pushd ${IMG_DOWNLOAD_DIR}
 kubeadm config images list > kubeadm-images.list
 for image in $(cat kubeadm-images.list); do 
   ctr image pull ${image}
-  ctr image export $(printf ${image}| tr ':/' '__') ${image}
+  ctr image export $(printf ${image}| tr ':/' '__').tar.gz ${image}
 done
 
 popd
---
+++ k8s/kubeadm/kubeadm-setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Prerequisite: Transfer files downloaded using kubeadm-download.sh to /opt on the target machine
+
+TOOLS_DOWNLOAD_DIR=/opt/k8s-tools
+IMG_DOWNLOAD_DIR=/opt/k8s-images
+BIN_DIR=/usr/local/bin
+DEP_DOWNLOAD_DIR=/opt/k8s-dependencies
+
+# Load modules
+lsmod | grep br_netfilter || bash -c "echo br_netfilter | tee /etc/modules-load.d/k8s.conf"
+lsmod | grep overlay || bash -c "echo overlay | tee /etc/modules-load.d/containerd.conf"
+
+modprobe overlay
+modprobe br_netfilter
+
+# Let iptables see bridged traffic
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl -w net.ipv4.ip_forward=1
+
+# Install kubectl, kubelet and kubeadm
+pushd ${TOOLS_DOWNLOAD_DIR}
+
+for t in "kubectl" "kubelet" "kubeadm";do sudo install -o root -g root -m 0755 ${t} /usr/local/bin/${t};done
+
+# Extract CRI, containerd, CNI files
+tar --no-overwrite-dir -C / -xzf cri-containerd-cni-*-linux-amd64.tar.gz
+systemctl daemon-reload
+systemctl start containerd
+
+popd
+
+# Load container images required for k8s cluster setup
+pushd ${IMG_DOWNLOAD_DIR}
+
+for image in $(ls *.gz); do 
+  ctr -n k8s.io image import ${image}
+done
+
+popd
+
+# Dependencies
+dpkg -i ${DEP_DOWNLOAD_DIR}/*.deb
+
+# kubeadm init
+kubeadm init
+
+# kubelet
+# pushd ${TOOLS_DOWNLOAD_DIR}
+
+# mkdir -p /etc/systemd/system/kubelet.service.d
+
+# cat kubelet.service | sed "s:/usr/bin:${BIN_DIR}:g" | tee /etc/systemd/system/kubelet.service
+
+# cat 10-kubeadm.conf | sed "s:/usr/bin:${BIN_DIR}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+# cat << EOF > /etc/systemd/system/kubelet.service.d/0-containerd.conf
+# [Service]
+# Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
+# EOF
+
+# systemctl enable --now kubelet
+
+# popd
