4d8802e8b24f03fadac80739cf8f3f29040a0188
S <S>
2022-03-20 16:33:59 +0400

    Added dataclass.py

---
+++ code/python/dataclass.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+
+@dataclass
+# (frozen=True)
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        """"Add 2 instances using +"""
+        return Point(
+            self.x + other.x,
+            self.y + other.y
+        )
+
+    def __sub__(self, other):
+        """"Substract 2 instances using -"""
+        return Point(
+            self.x - other.x,
+            self.y - other.y
+        )
+
+
+x = Point(1, 1)
+y = Point(3, 3)
+z = x + y
+print(z)
