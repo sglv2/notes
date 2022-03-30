d01c6a8b73a9c59df0b41b272e359cafae2e3d17
S <S>
2022-03-31 03:18:59 +0400

    Updated iterables-generators.py

--- code/python/iterables-generators.py
+++ code/python/iterables-generators.py
@@ -4,11 +4,34 @@
 def generate_first_n(n):
     i = 1
     while i <= n:
-        yield i
+        yield i * 10
         i += 1
 
 
 # sum function accepts an iterable
 print(sum(generate_first_n(5)))
-for i in generate_first_n(5):
-    print(i)
+for v in generate_first_n(5):
+    print(v)
+
+for i, v in enumerate(generate_first_n(5)):
+    print(f'index={i} value={v}')
+
+
+# Iterator
+# You have to implement __iter__() and __next__() methods
+class OddNumbers:
+    def __iter__(self):
+        self.n = 2
+        return self
+
+    def __next__(self):
+        x = self.n
+        self.n += 2
+        return x
+
+
+odd = OddNumbers()
+it = iter(odd)
+
+print(next(it))
+print(next(it))
