bf3f30e8a76a13ddd26e1ee17c65d1593f8c40e4
S <S>
2022-04-10 22:15:59 +0400

    Updated Python examples

---
+++ code/python/file.py
@@ -0,0 +1,37 @@
+try:
+    f1 = open('f1.txt', 'r')
+    for line in f1:
+        print(line)
+    f1.close()
+except FileNotFoundError:
+    print('File not found')
+
+f2 = open('f2.txt', 'w')
+f2.write("first\n")
+f2.write("second\n")
+f2.writelines(["third\n", "fourth\n"])
+f2.close()
+
+f2 = open('f2.txt', 'a')
+f2.write("fifth\n")
+f2.close()
+
+# CSV
+import csv
+
+with open('f3.csv', mode='r') as f3:
+    csv_content = csv.reader(f3)
+
+    for line_items in csv_content:
+        print(line_items)
+
+# JSON
+
+import json
+
+f4 = open('f4.json')
+data = json.load(f4)
+print(data)
+for k, v in data.items():
+    print(f'k={k} v={v}')
+f4.close()
