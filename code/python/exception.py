0055a279567caa59e525058a967b0bc65e2cca3c
S <S>
2022-04-25 05:54:39 +0400

    Updated Python examples

--- code/python/exception.py
+++ code/python/exception.py
@@ -2,19 +2,22 @@ import logging
 
 log = logging.getLogger(__name__)
 
+def log_error(message):
+    log.error(f'[{__name__}] {message}')
+
+
 l = [1, 2, 3, 4, 5]
 try:
     print(l[10])
 except IndexError:
-    log.error('IndexError')
-
+    log_error('IndexError')
 
 try:
     print(l[1])
 except IndexError:
-    log.error('IndexError')
+    log_error('IndexError')
 else:
-    log.error('Something else')
+    log_error('Something else')
 finally:
     print('Step that is always executed')
 
