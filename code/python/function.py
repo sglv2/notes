e5f9e1c66a39a88526e5759d4d82d4503eb9983d
S <S>
2022-04-27 20:08:32 +0400

    Updated Python examples

--- code/python/function.py
+++ code/python/function.py
@@ -1,5 +1,5 @@
 # Arbitrary arguments
-def sum1(*args):
+def sum1(*args) -> int:
     n = 0
     for i in range(len(args)):
         print(args[i])
@@ -18,3 +18,23 @@ def sum2(**kwargs):
 
 
 sum2(p1='v1', p2='v2', p3='v3')
+
+# Lambda
+double = lambda x: 2 * x
+print(double(10))
+
+## map - transform elements into new values
+m = map(lambda x: x * x, range(5))
+for e in m:
+    print(e)
+
+## filter - filter elements matching a condition
+numbers = [10, 12, 20, 22, 34, 48]
+num_ending_in_2 = filter(lambda x: x % 10 == 2, numbers)
+print(f'{list(num_ending_in_2)=}')
+
+## reduce
+from functools import reduce
+
+sum10 = reduce(lambda x, y: x + y, range(10))
+print(f'{sum10=}')
